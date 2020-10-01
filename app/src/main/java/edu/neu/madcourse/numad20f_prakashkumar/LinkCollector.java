f97df65c5803f30ddf0a1d84d57e5606491554c1
PrakashKumar <kumar.prak@northeastern.edu>
2020-09-30 20:24:25 -0400

    input textfield added

--- app/src/main/java/edu/neu/madcourse/numad20f_prakashkumar/LinkCollector.java
+++ app/src/main/java/edu/neu/madcourse/numad20f_prakashkumar/LinkCollector.java
@@ -3,6 +3,12 @@ package edu.neu.madcourse.numad20f_prakashkumar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LinkCollector extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class LinkCollector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
+
+        FloatingActionButton fab_add_link = findViewById(R.id.fab_addLink);
+        fab_add_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //Snackbar.make(view, "URL Added Successfully", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
