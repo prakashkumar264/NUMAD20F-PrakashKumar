0096d9a20c2b53d17ba76b214371689687239ff5
PrakashKumar <kumar.prak@northeastern.edu>
2020-10-06 15:44:34 -0400

    Latitude longitude Done

---
+++ app/src/main/java/edu/neu/madcourse/numad20f_prakashkumar/GetLocation.java
@@ -0,0 +1,92 @@
+package edu.neu.madcourse.numad20f_prakashkumar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class GetLocation extends AppCompatActivity {
+
+    TextView txtlat, txtLong;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_location);
+        txtlat = findViewById(R.id.txt_lat_answer);
+        txtLong = findViewById(R.id.txt_long_answer);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(GetLocation.this);
+        if(ActivityCompat.checkSelfPermission(GetLocation.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+             && ActivityCompat.checkSelfPermission(GetLocation.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            getCurrentLocation();
+        }else{
+            ActivityCompat.requestPermissions(GetLocation.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 100 && grantResults.length >0 && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)){
+            getCurrentLocation();
+        }else{
+            Toast.makeText(getApplicationContext(), "Permission Denied. Grant Permissions for Checking Location", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService((Context.LOCATION_SERVICE));
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+             fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                 @Override
+                 public void onComplete(@NonNull Task<Location> task) {
+                     Location location = task.getResult();
+                     if(location != null){
+                         txtlat.setText(String.valueOf(location.getLatitude()));
+                         txtLong.setText(String.valueOf(location.getLongitude()));
+                     }else{
+                         LocationRequest locationRequest = new LocationRequest()
+                                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                 .setInterval(10000)
+                                 .setFastestInterval(1000)
+                                 .setNumUpdates(1);
+                         LocationCallback locationCallback = new LocationCallback(){
+                             @Override
+                             public void onLocationResult(LocationResult locationResult){
+                                 Location location1 = locationResult.getLastLocation();
+                                 txtlat.setText(String.valueOf(location1.getLatitude()));
+                                 txtLong.setText(String.valueOf(location1.getLongitude()));
+                             }
+                         };
+                         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                     }
+                 }
+             });
+        }else{
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+}
