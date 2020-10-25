6c5cad79f90cd53f134e619c53dabbc50e90d87e
PrakashKumar <kumar.prak@northeastern.edu>
2020-10-25 00:35:20 -0400

    created basic layout

--- app/src/main/java/edu/neu/madcourse/numad20f_prakashkumar/activity_web_service.java
+++ app/src/main/java/edu/neu/madcourse/numad20f_prakashkumar/activity_web_service.java
@@ -2,13 +2,33 @@ package edu.neu.madcourse.numad20f_prakashkumar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_web_service extends AppCompatActivity {
+
+    private EditText editText_search;
+    private TextView textView_pronunciation;
+    private TextView textView_meaning;
+    private TextView textView_synonyms;
+    private TextView textView_example;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_service);
+
+        editText_search = findViewById(R.id.edittext_dic_name);
+        textView_pronunciation = findViewById(R.id.txtview_ponun_answer);
+        textView_meaning = findViewById(R.id.txtview_meaning_answer);
+        textView_synonyms = findViewById(R.id.txtview_synonyms_answer);
+        textView_example = findViewById(R.id.txtview_example_answer);
+
     }
+
+   
 }
